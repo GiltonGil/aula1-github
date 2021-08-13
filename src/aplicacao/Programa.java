3cdd15a0657080ceab022f76c691d5c19b7a7e72
Gilton Gil <giltongil11@gmail.com>
2021-08-12 22:11:09 -0300

    Projeto criado

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,16 @@
+
+package aplicacao;
+
+public class Programa {
+    
+    public static void main(String[] args) {
+        
+        String x = "Vamos juntos continuar apoiando os estudos ";
+        System.out.println(x);
+        
+        int y = 0;
+        System.out.println(x+1+y);
+        
+    }
+    
+}
